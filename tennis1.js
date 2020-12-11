ab76e1a05b19e83733fae35a53ef328c7b106142
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-11 22:42:45 +0100

    replaced strings with constants literals

--- tennis1.js
+++ tennis1.js
@@ -1,21 +1,12 @@
-'use strict';
-
-function getScore(playerOneScore, playerTwoScore) {
-    if (haveBothPlayerMoreThanThreePoints(playerOneScore, playerTwoScore)) {
-        return "Deuce";
-    }
-    if (haveSamePoints(playerOneScore, playerTwoScore)) {
-        return parseSingleScore(playerOneScore) + "-All"
-    }
-    if (haveAnyPlayerAdvantage(playerOneScore, playerTwoScore)) {
-        return calculatePlayerWinnerOrAdvantage(playerOneScore, playerTwoScore)
-    }
-
-    return parseSingleScore(playerOneScore) + "-" + parseSingleScore(playerTwoScore)
-
-}
-
-
+const DEUCE = "Deuce";
+const LOVE = "Love";
+const FIFTEEN = "Fifteen";
+const THIRTY = "Thirty";
+const FORTY = "Forty";
+const PLAYER_ONE_WINNER = "Win for player1"
+const PLAYER_ONE_ADVANTAGE = "Advantage player1"
+const PLAYER_TWO_WINNER = "Win for player2"
+const PLAYER_TWO_ADVANTAGE = "Advantage player2"
 
 function haveAnyPlayerAdvantage(playerOneScore, playerTwoScore) {
     return playerOneScore >= 4 || playerTwoScore >= 4
@@ -29,20 +20,32 @@ function haveBothPlayerMoreThanThreePoints (playerOneScore, playerTwoScore) {
     return haveSamePoints(playerOneScore, playerTwoScore) && playerOneScore >= 3
 }
 function parseSingleScore(scoreNumber) {
-    if (scoreNumber === 0) return "Love"
-    if (scoreNumber === 1) return "Fifteen"
-    if (scoreNumber === 2) return "Thirty"
+    if (scoreNumber === 0) return LOVE
+    if (scoreNumber === 1) return FIFTEEN
+    if (scoreNumber === 2) return THIRTY
 
-    return "Forty"
+    return FORTY
 }
 
 function calculatePlayerWinnerOrAdvantage(playerOneScore, playerTwoScore) {
     const countPointDifferences = playerOneScore - playerTwoScore;
-    if (countPointDifferences === 1) return "Advantage player1";
-    if (countPointDifferences === -1) return "Advantage player2";
-    if (countPointDifferences >= 2) return "Win for player1";
+    if (countPointDifferences === 1) return PLAYER_ONE_ADVANTAGE;
+    if (countPointDifferences === -1) return PLAYER_TWO_ADVANTAGE;
+    if (countPointDifferences >= 2) return PLAYER_ONE_WINNER;
 
-    return "Win for player2";
+    return PLAYER_TWO_WINNER;
 }
 
-module.exports = getScore;
+module.exports = function getScore(playerOneScore, playerTwoScore) {
+    if (haveBothPlayerMoreThanThreePoints(playerOneScore, playerTwoScore)) {
+        return DEUCE;
+    }
+    if (haveSamePoints(playerOneScore, playerTwoScore)) {
+        return parseSingleScore(playerOneScore) + "-All"
+    }
+    if (haveAnyPlayerAdvantage(playerOneScore, playerTwoScore)) {
+        return calculatePlayerWinnerOrAdvantage(playerOneScore, playerTwoScore)
+    }
+
+    return parseSingleScore(playerOneScore) + "-" + parseSingleScore(playerTwoScore)
+}
