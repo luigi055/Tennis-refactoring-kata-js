3c23f416b7e575e46685249dc62fb9b5e1481ca5
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-12 23:17:54 +0100

    replace type code with ADT

--- tennis2.js
+++ tennis2.js
@@ -1,29 +1,51 @@
-'use strict';
+// Mimic Algebraic Data Type in Javascript to Represent a Score
+class Score {
+    static LOVE = "Love";
+    static FIFTEEN = "Fifteen";
+    static THIRTY = "Thirty";
+    static FORTY = "Forty";
+    static ADVANTAGE = "Advantage";
+    constructor(scoreNumber) {
+        this.scoreNumber = scoreNumber
+    }
 
+    static parse({scoreNumber}) {
+        const {LOVE, FIFTEEN, THIRTY, FORTY} = this
+        if (scoreNumber === 0) return LOVE;
+        if (scoreNumber === 1) return FIFTEEN;
+        if (scoreNumber === 2) return THIRTY;
+        return FORTY;
+    }
 
-const haveSameScore = (p1,p2) => p1 === p2
-const isDeuce = (p1,p2) => haveSameScore(p1,p2) && p1 > 2
-const hasAnyPlayerAdvantage = (p1,p2) => p1 >= 4 || p2 >= 4
-const isThereWinner = (p1,p2) => hasAnyPlayerAdvantage(p1,p2) && (Math.abs(p1 - p2)) >= 2
-function parseScore(scoreNumber) {
-    if (scoreNumber === 0) return "Love"
-    if (scoreNumber === 1) return "Fifteen"
-    if (scoreNumber === 2) return "Thirty";
-    return "Forty";
-}
-const getPlayerScoresWithdraw = (p1,p2) => isDeuce(p1,p2) ? "Deuce" :`${parseScore(p1)}-All`
-const getWinningPlayer = (p1, p2) => p1 > p2 ? "player1":"player2"
-
-function getScore(P1point, P2point) {
-    const winningPlayer = getWinningPlayer(P1point,P2point);
-
-    if (isThereWinner(P1point,P2point)) return `Win for ${winningPlayer}`;
-    if (haveSameScore(P1point,P2point)) return getPlayerScoresWithdraw(P1point,P2point);
-    if (hasAnyPlayerAdvantage(P1point, P2point)) return `Advantage ${winningPlayer}`;
-
-    return  parseScore(P1point) + "-" +  parseScore(P2point);
-
+    static areEquals(scoreOne, scoreTwo) {
+        return scoreOne.scoreNumber === scoreTwo.scoreNumber
+    }
 }
 
+const isDeuce = (playerOneScore,playerTwoScore) =>
+    Score.areEquals(playerOneScore,playerTwoScore) && playerOneScore.scoreNumber > 2;
+const hasAnyPlayerAdvantage = (playerOneScore,playerTwoScore) =>
+    playerOneScore.scoreNumber >= 4 || playerTwoScore.scoreNumber >= 4;
+const isThereWinner = (playerOneScore,playerTwoScore) =>
+    hasAnyPlayerAdvantage(playerOneScore,playerTwoScore)
+    && (Math.abs(playerOneScore.scoreNumber - playerTwoScore.scoreNumber)) >= 2;
+const getPlayerScoresWithdraw = (playerOneScore,playerTwoScore) =>
+    isDeuce(playerOneScore,playerTwoScore) ? "Deuce" :`${Score.parse(playerOneScore)}-All`;
+const getWinningPlayer = (playerOneScore, playerTwoScore) =>
+    playerOneScore > playerTwoScore ? "player1":"player2";
 
-module.exports = getScore;
+module.exports = function getScore(playerOneScoreNumber, playerTwoScoreNumber) {
+    const playerOne = new Score(playerOneScoreNumber);
+    const playerTwo = new Score(playerTwoScoreNumber);
+    const winningPlayer = getWinningPlayer(playerOneScoreNumber,playerTwoScoreNumber);
+    const {ADVANTAGE} = Score
+
+    if (isThereWinner(playerOne,playerTwo)) return `Win for ${winningPlayer}`;
+    if (Score.areEquals(playerOne,playerTwo))
+        return getPlayerScoresWithdraw(playerOne,playerTwo);
+    if (hasAnyPlayerAdvantage(playerOne, playerTwo)) 
+        return `${ADVANTAGE} ${winningPlayer}`;
+
+    return  `${Score.parse(playerOne)}-${Score.parse(playerTwo)}`;
+
+}
