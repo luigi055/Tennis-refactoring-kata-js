8a233e4fa1ada88e88cc9fde33acdae9b0dd12e5
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-12 22:24:54 +0100

    consolidate conditional expression

--- tennis2.js
+++ tennis2.js
@@ -3,8 +3,8 @@
 
 const haveSameScore = (p1,p2) => p1 === p2
 const isDeuce = (p1,p2) => haveSameScore(p1,p2) && p1 > 2
-const hasAnyPlayerAdvantage = (p1,p2) => p1 >= 3 && p2 >= 3
-const isPlayerWonTo = (p1,p2) => p1 >= 4 && p2 >= 0 && (p1 - p2) >= 2
+const hasAnyPlayerAdvantage = (p1,p2) => p1 >= 4 || p2 >= 4
+const isThereWinner = (p1,p2) => hasAnyPlayerAdvantage(p1,p2) && (Math.abs(p1 - p2)) >= 2
 function parseScore(scoreNumber) {
     if (scoreNumber === 0) return "Love"
     if (scoreNumber === 1) return "Fifteen"
@@ -14,8 +14,7 @@ function parseScore(scoreNumber) {
 const getPlayerScoresWithdraw = (p1,p2) => isDeuce(p1,p2) ? "Deuce" :`${parseScore(p1)}-All`
 
 function getScore(P1point, P2point) {
-    if (isPlayerWonTo(P1point,P2point)) return "Win for player1";
-    if (isPlayerWonTo(P2point,P1point)) return "Win for player2";
+    if (isThereWinner(P1point,P2point)) return P1point > P2point ? "Win for player1" : "Win for player2";
     if (haveSameScore(P1point,P2point)) return getPlayerScoresWithdraw(P1point,P2point);
     if (hasAnyPlayerAdvantage(P1point, P2point))
         return P1point > P2point ? "Advantage player1" : "Advantage player2";
