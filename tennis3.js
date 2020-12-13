bace99b0b641f9fdf610a4a08dd177c73781c9df
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-13 17:43:39 +0100

    consolidate conditional expressionin final ternary expression

--- tennis3.js
+++ tennis3.js
@@ -13,6 +13,8 @@ const PLAYER_TWO = "player2"
 function getScore(p1, p2) {
     const p = [LOVE, FIFTEEN, THIRTY, FORTY];
     const winningPlayer = p1 > p2 ? PLAYER_ONE : PLAYER_TWO
+    const isThereAdvantage = () => Math.abs(p1 - p2) === 1
+
     if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6)) {
         let s;
         s = p[p1];
@@ -21,7 +23,7 @@ function getScore(p1, p2) {
         if (p1 === p2) {
             return DEUCE;
         }
-        return `${(Math.abs(p1 - p2) === 1)
+        return `${isThereAdvantage()
                     ? ADVANTAGE
                     : "Win for"
                 } ${winningPlayer}`;
