1c16daa23d352c214e290615795b560ceeb5bf8c
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-13 17:34:53 +0100

    extract primitive strings to constants

--- tennis3.js
+++ tennis3.js
@@ -1,19 +1,26 @@
 'use strict';
 
+const LOVE = "Love";
+const FIFTEEN = "Fifteen";
+const THIRTY = "Thirty";
+const FORTY = "Forty";
+const DEUCE = "Deuce";
+const ADVANTAGE = "Advantage"
+
 function getScore(p1, p2) {
     var s;
     let p1N = "player1";
     let p2N = "player2";
     if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6)) {
-        var p = ["Love", "Fifteen", "Thirty", "Forty"];
+        var p = [LOVE, FIFTEEN, THIRTY, FORTY];
         s = p[p1];
         return (p1 === p2) ? s + "-All" : s + "-" + p[p2];
     } else {
         if (p1 === p2) {
-            return "Deuce";
+            return DEUCE;
         }
         s = p1 > p2 ? p1N : p2N;
-        return ((p1 - p2) * (p1 - p2) === 1) ? "Advantage " + s : "Win for " + s;
+        return ((p1 - p2) * (p1 - p2) === 1) ? ADVANTAGE + " " + s : "Win for " + s;
     }
 }
 
